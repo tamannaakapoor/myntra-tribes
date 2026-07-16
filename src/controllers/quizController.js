b0a636bb433ec03b7197c3d010a74ebf3731c13b
Aditico <aditimaurya.1217@gmail.com>
2026-07-17 03:24:22 +0530

    Add quiz submit endpoint

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -1,4 +1,18 @@
+// const quizQuestions = require("../data/quizQuestions");
+
+// const getQuizQuestions = async (req, res) => {
+//   return res.status(200).json({
+//     success: true,
+//     count: quizQuestions.length,
+//     questions: quizQuestions,
+//   });
+// };
+
+// module.exports = {
+//   getQuizQuestions,
+// };
 const quizQuestions = require("../data/quizQuestions");
+const { assignUserTribe } = require("./tribeController");
 
 const getQuizQuestions = async (req, res) => {
   return res.status(200).json({
@@ -10,4 +24,5 @@ const getQuizQuestions = async (req, res) => {
 
 module.exports = {
   getQuizQuestions,
+  submitQuiz: assignUserTribe,
 };
--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,11 +1,27 @@
+// const express = require("express");
+
+// const router = express.Router();
+
+// const {
+//   getQuizQuestions,
+// } = require("../controllers/quizController");
+
+// router.get("/questions", getQuizQuestions);
+
+// module.exports = router;
+
 const express = require("express");
 
 const router = express.Router();
 
 const {
   getQuizQuestions,
+  submitQuiz,
 } = require("../controllers/quizController");
 
 router.get("/questions", getQuizQuestions);
+
+// Frontend calls this endpoint
+router.post("/submit", submitQuiz);
 
 module.exports = router;
