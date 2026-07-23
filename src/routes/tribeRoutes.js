abcea85c73094853f4262add1f57e4cd92732076
Aditico <aditimaurya.1217@gmail.com>
2026-07-23 21:56:28 +0530

    changes for tribe

--- src/routes/tribeRoutes.js
+++ src/routes/tribeRoutes.js
@@ -9,7 +9,7 @@
 // router.post("/assign", assignUserTribe);
 
 // module.exports = router;
-
+const authenticateUser = require("../middleware/authMiddleware");
 const express = require("express");
 
 const router = express.Router();
@@ -19,14 +19,21 @@ const {
   getAllTribes,
   getSingleTribe,
   getTribeProducts,
+    selectTribe,
+
 } = require("../controllers/tribeController");
 
-router.post("/assign", assignUserTribe);
+// router.post("/assign", assignUserTribe);
+router.post(
+    "/assign",
+    authenticateUser,
+    assignUserTribe
+);
 
 router.get("/", getAllTribes);
 
 router.get("/:slug", getSingleTribe);
 
 router.get("/:slug/products", getTribeProducts);
-
+router.post("/select", authenticateUser, selectTribe);
 module.exports = router;
